50387602094bad9f179a354aa6ee21650c47d5c4
benweitzman <benjamin.weitzman@tufts.edu>
2012-10-10 13:53:02 -0400

    switched to AudioStreamer player

---
+++ PadSonic/SelectMusicFolderViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelectMusicFolderViewController.h
+//  PadSonic
+//
+//  Created by Ben Weitzman on 10/9/12.
+//  Copyright (c) 2012 Ben Weitzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectMusicFolderViewController : UITableViewController
+
+@end
